9fe06a48e9fede3e86caf35e0bc7075dd75bdd8c
maksuta <dartmakstlt@gmail.com>
2026-08-13 19:16:35 +0300

    release add oneself track and 1.0 bot

--- data_base/base.py
+++ data_base/base.py
@@ -17,15 +17,16 @@ async def init_base():
                 yandex_id_file INTEGER UNIQUE,
                 file_id TEXT UNIQUE,
                 track_name TEXT,
-                artist_name TEXT
+                artist_name TEXT,
+                year INTEGER
             )
                         """)
         await db.execute("""
             CREATE TABLE IF NOT EXISTS favorite_tracks(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 track_id INTEGER,
-                id_from_users INTEGER,
-                FOREIGN KEY (id_from_users) REFERENCES users (user_id)
+                user_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users (id),
                 FOREIGN KEY (track_id) REFERENCES track_base (id)
             )
                         """)
@@ -36,13 +37,61 @@ async def add_user(user_id,username = "NULL"):
         await db.execute("INSERT INTO users(user_id,username) SELECT ?,? WHERE NOT EXISTS(SELECT 1 FROM users WHERE user_id = ?)",(user_id,username,user_id))
         await db.commit()
 
-async def add_track(yandex_id_file,file_id,track_name,artist_name):
+async def add_track(yandex_id_file,file_id,track_name,artist_name,year = 0):
     async with aiosqlite.connect(DB_NAME) as db:
-        await db.execute("INSERT INTO track_base(yandex_id_file,file_id,track_name,artist_name) SELECT ?,?,?,? WHERE NOT EXISTS(SELECT 1 FROM track_base WHERE yandex_id_file = ?)",(yandex_id_file,file_id,track_name,artist_name,yandex_id_file))
+        await db.execute("INSERT INTO track_base(yandex_id_file,file_id,track_name,artist_name,year) SELECT ?,?,?,?,? WHERE NOT EXISTS(SELECT 1 FROM track_base WHERE yandex_id_file = ?)",(yandex_id_file,file_id,track_name,artist_name,year,yandex_id_file))
         await db.commit()
 
 async def check_track(yandex_id_file):
     async with aiosqlite.connect(DB_NAME) as db:
-        object = await db.execute("SELECT file_id,track_name,artist_name FROM track_base WHERE yandex_id_file = ?",(yandex_id_file,))
+        object = await db.execute("SELECT file_id,track_name,artist_name,year FROM track_base WHERE yandex_id_file = ?",(yandex_id_file,))
         track_info = await object.fetchone()
         return track_info
+
+async def add_in_playlist(yandex_id,telegram_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        object_1 = await db.execute("SELECT id FROM users WHERE user_id = ?",(telegram_id,))
+        object_2 = await db.execute("SELECT id FROM track_base WHERE yandex_id_file = ?",(yandex_id,))
+        user_row = await object_1.fetchone()
+        track_row = await object_2.fetchone()
+        if not user_row or not track_row:
+            return None
+        user_id = user_row[0]
+        track_id = track_row[0]
+        is_check = await db.execute("SELECT count(track_id) FROM favorite_tracks WHERE user_id = ? ",(user_id,))
+        if (await is_check.fetchone())[0] >= 10:
+            return "full"
+        cursor = await db.execute("SELECT 1 FROM favorite_tracks WHERE track_id = ? AND user_id = ?",(track_id,user_id))
+        if await cursor.fetchone():
+            await db.execute("DELETE FROM favorite_tracks WHERE track_id = ? AND user_id = ?",(track_id,user_id))
+            await db.commit()
+        await db.execute("INSERT INTO favorite_tracks(track_id,user_id) VALUES(?,?)",(track_id,user_id))
+        await db.commit()
+        return True
+
+async def get_list_file(telegram_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        sql = await db.execute("SELECT track_base.file_id FROM track_base " \
+        "JOIN favorite_tracks ON track_base.id = favorite_tracks.track_id " \
+        "JOIN users ON favorite_tracks.user_id = users.id " \
+        "WHERE users.user_id = ? " \
+        "ORDER BY favorite_tracks.id DESC",(telegram_id,)) 
+        list_id = await sql.fetchall()
+        return [x[0] for x in list_id]
+
+async def get_list_track(telegram_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        sql = await db.execute("SELECT track_base.yandex_id_file,track_base.track_name,track_base.artist_name FROM track_base " \
+                "JOIN favorite_tracks ON track_base.id = favorite_tracks.track_id " \
+                "JOIN users ON favorite_tracks.user_id = users.id " \
+                "WHERE users.user_id = ? " \
+                "ORDER BY favorite_tracks.id DESC",(telegram_id,)) 
+        track_list = await sql.fetchall()
+        return track_list
+
+async def delete_track(telegram_id,yandex_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("DELETE FROM favorite_tracks " \
+        "WHERE favorite_tracks.user_id = (SELECT users.id FROM users WHERE users.user_id = ?) " \
+        "AND favorite_tracks.track_id = (SELECT track_base.id FROM track_base WHERE track_base.yandex_id_file = ?)",(telegram_id,int(yandex_id)))
+        await db.commit()
--- processing_message/router.py
+++ processing_message/router.py
@@ -1,26 +1,29 @@
 from aiogram import Router,F
 from aiogram.filters import Command
-from aiogram.types import Message,BufferedInputFile,InlineKeyboardButton,callback_query,CallbackQuery
+from aiogram.types import Message,BufferedInputFile,InlineKeyboardButton,InlineKeyboardMarkup,callback_query,CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from data_base.base import add_user,add_track,check_track
+from data_base.base import add_user,add_track,check_track,add_in_playlist,get_list_file,get_list_track,delete_track
 import re
 from yandex_music import ClientAsync
-
+from aiogram.types.input_media_audio import InputMediaAudio
 router = Router()
 
 pattern = r'track/(\d+)'
 
 COUNT_ANSWER = 5
 
+user_buffer = set()
+
 async def download_track(yandex_id:str,yandex_client:ClientAsync):
     try:
         full_track = await yandex_client.tracks(yandex_id)
         if full_track:
             track_bytes = await full_track[0].download_bytes_async(codec="mp3")
-            title = full_track[0].title
-            artist = ",".join(list(map(lambda c:c.name,full_track[0].artists)))
-            audio_file = BufferedInputFile(track_bytes,filename=f"{title}.mp3")
-            track = {"audio":audio_file,"title":title,"artist":artist}
+            title = full_track[0].title if full_track[0].title else "Без названия"
+            artist = ",".join(list(map(lambda c:c.name,full_track[0].artists))) if full_track[0].artists else "Неизвестный исполнитель"
+            audio_file = BufferedInputFile(track_bytes,filename=f"{title}.mp3") 
+            year = full_track[0].albums[0].year if full_track[0].albums else 0
+            track = {"audio":audio_file,"title":title,"artist":artist,"year":year}
         else:return None
     except Exception as e:
         print(e)
@@ -29,78 +32,180 @@ async def download_track(yandex_id:str,yandex_client:ClientAsync):
 
 @router.message(Command("start"))
 async def start_bot(message:Message):
-    await message.answer(f"Привет,{message.from_user.first_name}\nЭто бот для удобного скачивания и прослушивания твои любимых треков\nОтправь мне ссылку на трек с яндекс музыки или напиши примерное название")
+    await message.answer(f"Привет,{message.from_user.first_name}!")
+    await message.answer("Это бот для удобного скачивания и прослушивания твои любимых треков\n\nСписок того,что умеет бот:\n1) превратить ссылку на трек в файл\n2) после обычного текста представить список вариантов треков с таким названием для скачки\n3) /favourite_list - вывести список избранных треков\n4) /start - перезагрузить бота\n5) /delete - удалить песню из избранного\n6) Отправьте файл",parse_mode="HTML")
     await add_user(message.from_user.id,message.from_user.username)
-    await message.answer("Извините,бот пока не работает,сохраните его где-нибудь,я обещаю сделать его,стараюсь каждый день,можете лично написать\n@maksggg_bs")
+
+@router.message(Command("favourite_list"))
+async def print_list(message:Message):
+    try:
+        telegram_id = message.from_user.id
+        track_list = await get_list_file(telegram_id)
+        if not track_list:
+            await message.answer("В избранном нету треков")
+            return
+        media_group = list(InputMediaAudio(media=x) for x in track_list)
+        await message.answer_media_group(media=media_group)
+    except Exception as e:
+        await message.answer("Ошибка при выводе избранного")
+        print(e)
+
+@router.message(Command("delete"))
+async def delete_command(message:Message):
+    try:
+        user_id = message.from_user.id
+        list_track = await get_list_track(user_id)
+        if not list_track:
+            await message.answer("Список избранного пуст")
+            return
+        builder = InlineKeyboardBuilder()
+        for track in list_track:
+            yandex_id = track[0]
+            title = track[1]
+            artist = track[2]
+            builder.add(InlineKeyboardButton(text=f"{title} - {artist}",callback_data=f"del:{yandex_id}"))
+        builder.adjust(1)
+        await message.answer("Нажмите на трек для удаления",reply_markup=builder.as_markup())
+    except Exception as e:
+        await message.answer("Ошибка при удалении")
+        print(e)
 
 @router.message(F.text)
 async def processing(message:Message,yandex_client:ClientAsync):
     src = re.search(pattern,message.text)
-
+    id = message.from_user.id
+    if id in user_buffer:
+        await message.answer("Не спамь,пожалуйста,бот выполняет команду")
+        return
     if src:
         yandex_id = src.group()[6:]
         try:
+            user_buffer.add(id)
             track_info = await check_track(int(yandex_id))
             if track_info:
                 file_id = track_info[0]
                 title = track_info[1]
                 artist = track_info[2]
-                await message.answer_audio(audio=file_id,title=title,performer=artist)
+                year = f"{track_info[3]}г" if track_info[3] else ""
+                await message.answer_audio(audio=file_id,title=title,performer=artist,caption=year,reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Текст песни",callback_data=f"text:{yandex_id}"),InlineKeyboardButton(text="В избранное",callback_data=f"add:{yandex_id}")]]))
                 return
             else:
                 track = await download_track(yandex_id,yandex_client)
                 if track:
-                    send_message = await message.answer_audio(audio=track["audio"],title=track["title"],performer=track["artist"])
-                    await add_track(int(yandex_id),send_message.audio.file_id,track["title"],track["artist"])
+                    year = f"{track["year"]}г" if track["year"] else "" 
+                    send_message = await message.answer_audio(audio=track["audio"],title=track["title"],performer=track["artist"],caption=year,reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Текст песни",callback_data=f"text:{yandex_id}"),InlineKeyboardButton(text="В избранное",callback_data=f"add:{yandex_id}")]]))
+                    await add_track(int(yandex_id),send_message.audio.file_id,track["title"],track["artist"],track["year"])
                 else:
                     await message.answer("трек не нашелся")
         except Exception as e:
             await message.answer("сыллка битая или Яндекс лох")
             print(e)
-        return
+        finally:
+            user_buffer.discard(id)
+            return
     try:
+        user_buffer.add(id)
         res = await yandex_client.search(text = str(message.text),type_="track")    
         if res.tracks and res.tracks.results:
             full_tracks = list(res.tracks.results[:COUNT_ANSWER])
             if full_tracks: 
-                builder = InlineKeyboardBuilder()
+                builder_list = InlineKeyboardBuilder()
                 for track in full_tracks:
-                    builder.add(InlineKeyboardButton(text=f"{track.title} - {track.artists[0].name}",callback_data=f"id:{track.id}"))
-                builder.adjust(1)
-                await message.answer("Выберите вариант из предложенных",reply_markup=builder.as_markup())
+                    builder_list.add(InlineKeyboardButton(text=f"{track.title} - {track.artists[0].name}",callback_data=f"id:{track.id}"))
+                builder_list.adjust(1)
+                await message.answer("Выберите вариант из предложенных",reply_markup=builder_list.as_markup())
             else: await message.answer("поиск сработал,но трек без полной версии(нельзя качать)")
         else:
             await message.answer("Поиск ничего не нашел")
     except Exception as e:
         await message.answer("Поиск лег или Яндекс лох")
         print(e)
-    return
+    finally:
+        user_buffer.discard(id)
+        return
+
+@router.message(F.audio)
+async def get_audio(message:Message):
+    try:
+        file_id = message.audio.file_id
+        title = message.audio.title if message.audio.title else "Без навзания"
+        artist = message.audio.performer if message.audio.performer else "Неизвестный исполнитель"
+        fake_yandex_id = -(message.from_user.id)
+        await add_track(fake_yandex_id,file_id,title,artist)
+        await message.answer(text="Трек загружен",reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Добавить в плейлист",callback_data=f"add:{fake_yandex_id}")]]))
+    except Exception as e:
+        await message.answer("Ошибка при загрузке трека")
+        print(e)
 
 @router.callback_query(F.data[:3] == "id:")
 async def download_inline(callback:CallbackQuery,yandex_client:ClientAsync):
     yandex_id = callback.data[3:]
-    track_info = await check_track(int(yandex_id))
-    if track_info:
-        try:
+    try:
+        track_info = await check_track(int(yandex_id))
+        if track_info:
             file_id = track_info[0]
             title = track_info[1]
             artist = track_info[2]
-            await callback.message.answer_audio(audio=file_id,title=title,performer=artist)
-        except:
-            await callback.message.answer("Ошибка скачивания")
-    else:
-        try:
-            track = await download_track(yandex_id,yandex_client)
-        except:
-            await callback.message.answer("Ошибка скачивания")
-        if track:
-            try:
-                send_message = await callback.message.answer_audio(audio=track["audio"],title=track["title"],performer=track["artist"])
-                await add_track(int(yandex_id),send_message.audio.file_id,track["title"],track["artist"])
-            except Exception as e:
-                await callback.message.answer("Ошибка отправки")
+            year = f"{track_info[3]}г" if track_info[3] else ""
+            await callback.message.answer_audio(audio=file_id,title=title,performer=artist,caption=year,reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Текст песни",callback_data=f"text:{yandex_id}"),InlineKeyboardButton(text="В избранное",callback_data=f"add:{yandex_id}")]]))
         else:
-            await callback.message.answer("не нашелся трек")
-    await callback.answer("")
+            track = await download_track(yandex_id,yandex_client)
+            if track:
+                year = f"{track["year"]}г" if track["year"] else ""
+                send_message = await callback.message.answer_audio(audio=track["audio"],title=track["title"],performer=track["artist"],caption=year,reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Текст песни",callback_data=f"text:{yandex_id}"),InlineKeyboardButton(text="В избранное",callback_data=f"add:{yandex_id}")]]))
+                await add_track(int(yandex_id),send_message.audio.file_id,track["title"],track["artist"],track["year"])
+            else:
+                await callback.message.answer("не нашелся трек")
+    except Exception as e:
+        await callback.message.answer("Ошибка скачивания")
+        print(e)
+    finally:
+        await callback.answer("")
 
-    
+@router.callback_query(F.data[:5] == "text:")
+async def give_text(callback:CallbackQuery,yandex_client:ClientAsync):
+    try:
+        yandex_id = callback.data[5:]
+        track_info = await yandex_client.tracks_lyrics(yandex_id)
+        if track_info:
+            text = await track_info.fetch_lyrics_async()
+            title = callback.message.audio.title if callback.message.audio else "Без названия"
+            artist = callback.message.audio.performer if callback.message.audio else "Неизвестный исполнитель"
+            year = callback.message.caption + "." if callback.message.caption else ""
+            await callback.message.answer(f"{title}\nТрек - {artist} {year}\n\nТекст песни\n\n{text}")
+    except Exception as e:
+        await callback.message.answer("Текст песни не найден")
+        print(e)
+    finally:
+        await callback.answer("")
+
+@router.callback_query(F.data[:4] == "add:")
+async def favourite_add(callback:CallbackQuery):
+    try:
+        yandex_id = callback.data[4:]
+        user_id = callback.from_user.id
+        flag = await add_in_playlist(yandex_id,user_id)
+        if not flag:
+            await callback.message.answer("Не нашлось такой песни/пользователя")
+        elif flag == "full":
+            await callback.message.answer("Лимит избранного 10 треков")
+        else:
+            await callback.message.answer("Трек добавлен")
+    except Exception as e:
+        await callback.message.answer("Ошибка при добавлении в избранное")
+        print(e)
+    finally:
+        await callback.answer("")
+
+@router.callback_query(F.data[:4] == "del:")
+async def delete_track_callback(callback:CallbackQuery):
+    try:
+        yandex_id = callback.data[4:]
+        user_id = callback.from_user.id
+        await delete_track(user_id,yandex_id)
+        await callback.message.answer("Трек удален")
+    except Exception as e:
+        await callback.message.answer("Ошибка при удалении")
+        print(e)
+    finally:
+        await callback.answer("")
